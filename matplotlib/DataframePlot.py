88d88b75d1399fdbde5af94417edb6f3d975f20b
rasel-093 <rasel.cse19.ruet@gmail.com>
2024-05-25 07:31:07 +0600

    plot coordinate points, dataframe

---
+++ matplotlib/DataframePlot.py
@@ -0,0 +1,24 @@
+#Plot a line with dataframe
+#Dataframe -> 2 dimenstional tabular data
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Data
+data = {
+    "Cricket_Bat": ["SG", "BDM", "SS", "GM", "KUKABURRA", "SPARTAN"],
+    "MRP": [2000,2200,2400,2500,2600,3000],
+    "Weight_grams": [1100,1200,1250,1330,1480,1600]
+}
+
+#Dataframe
+dataFrame = pd.DataFrame(data)
+
+#Plot a line using the pyplot
+#The x and y coordinates are the columns of the dataframe
+
+plt.plot(dataFrame["MRP"], dataFrame["Weight_grams"])
+
+#Add grid lines
+plt.grid()
+#Display the figure
+plt.show()
---
+++ matplotlib/PointPlot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Plotting co-ordinates
+#x points and y points for co-ordinates
+xpoints = np.array([0, 10])
+ypoints = np.array([0, 125])
+
+#Plot using the pyplot submodule
+plt.plot(xpoints, ypoints)
+#Display to plot
+plt.show()
+
